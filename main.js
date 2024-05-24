697eece6ddfe6189fcb77e43b0fd696e685b879b
Douglas <87150906+dlsr00t@users.noreply.github.com>
2024-05-24 17:54:46 -0300

    24/05/2024

---
+++ main.js
@@ -0,0 +1,22 @@
+//4002-8922 :(
+//0402-8922 :(
+//8002-8922 :(
+//0802-8922 :(
+//0902-8922 :(
+//9002-8922 :(
+//0002-8922 :(
+//002-8922 :)
+//
+
+
+import './style.css'
+//import javascriptLogo from './javascript.svg'
+//import viteLogo from '/vite.svg'
+//import { setupCounter } from './counter.js'
+
+
+
+setupCounter(document.querySelector('#counter'))
+
+
+
